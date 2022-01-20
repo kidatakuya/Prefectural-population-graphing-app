f628035936546f508be33ec3ddd2bed64a11e3d1
kidatakuya <kida.takuya.1234@gmail.com>
2022-01-20 23:20:57 +0900

    Graph display completed. (Adopted Highcharts)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'google',
+    'react-app',
+    'react-app/jest',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'google',
-        'react-app',
-        'react-app/jest',
-    ],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['react'],
-    rules: {
-        'operator-linebreak': ['error', 'before'],
-        'indent': ['error', 4],
-        'require-jsdoc': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/prop-types': 'off',
-        'object-curly-spacing': [4, 'always'],
-        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'operator-linebreak': ['error', 'before'],
+    'indent': [4, 4, { 'SwitchCase': 1 }],
+    'require-jsdoc': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'object-curly-spacing': [2, 'always'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
 };
--- src/components/Organisms/LineGraph/index.jsx
+++ src/components/Organisms/LineGraph/index.jsx
@@ -2,12 +2,71 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import './index.scss';
 function LineGraph() {
-    return (
-        <div>
-            <h2>グラフ</h2>
-            <HighchartsReact highcharts={Highcharts} />
-        </div>
-    );
+  const options = {
+    title: {
+      text: '総人口推移',
+    },
+    xAxis: {
+      categories: [
+        '1月',
+        '2月',
+        '3月',
+        '4月',
+        '5月',
+        '6月',
+        '7月',
+        '8月',
+        '9月',
+        '10月',
+        '11月',
+        '12月',
+      ],
+    },
+    yAxis: {
+      title: {
+        text: '人口',
+      },
+      plotLines: [
+        {
+          value: 0,
+          width: 1,
+          color: '#808080',
+        },
+      ],
+    },
+    series: [
+      {
+        name: '大阪',
+        data: [1, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2],
+      },
+      {
+        name: 'なら',
+        data: [3, 2, 1],
+      },
+    ],
+    responsive: {
+      rules: [
+        {
+          condition: {
+            maxWidth: 600,
+          },
+          chartOptions: {
+            legend: {
+              layout: 'horizontal',
+              align: 'center',
+              verticalAlign: 'bottom',
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
 }
 
 export default LineGraph;
