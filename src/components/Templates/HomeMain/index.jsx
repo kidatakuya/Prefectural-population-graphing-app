759a1ae0cb751340fa2d0202213fb91a02fe7b3e
kidatakuya <kida.takuya.1234@gmail.com>
2022-01-21 09:25:24 +0900

    Deleted the LineGraph component and placed and changed HighchartsReact in the HomeMain component.

--- src/components/Templates/HomeMain/index.jsx
+++ src/components/Templates/HomeMain/index.jsx
@@ -1,8 +1,68 @@
 // import React, { useState } from 'react';
-import { LineGraph } from './../../index';
-
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import './index.scss';
 function HomeMain() {
   const testList = ['奈良県', '大阪府', '京都府', '兵庫県'];
+  const options = {
+    title: {
+      text: '総人口推移',
+    },
+    xAxis: {
+      categories: [
+        '1月',
+        '2月',
+        '3月',
+        '4月',
+        '5月',
+        '6月',
+        '7月',
+        '8月',
+        '9月',
+        '10月',
+        '11月',
+        '12月',
+      ],
+    },
+    yAxis: {
+      title: {
+        text: '人口数',
+      },
+      plotLines: [
+        {
+          value: 0,
+          width: 1,
+          color: '#808080',
+        },
+      ],
+    },
+    series: [
+      {
+        name: '大阪',
+        data: [1, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2],
+      },
+      {
+        name: 'なら',
+        data: [3, 2, 1],
+      },
+    ],
+    responsive: {
+      rules: [
+        {
+          condition: {
+            maxWidth: 600,
+          },
+          chartOptions: {
+            legend: {
+              layout: 'horizontal',
+              align: 'center',
+              verticalAlign: 'bottom',
+            },
+          },
+        },
+      ],
+    },
+  };
   return (
     <main>
       <ul>
@@ -14,7 +74,7 @@ function HomeMain() {
             ))
           : ''}
       </ul>
-      <LineGraph />
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </main>
   );
 }
