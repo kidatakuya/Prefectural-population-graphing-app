4eb9654951d2df6ef4b201fe6755824b82403550
kidatakuya <kida.takuya.1234@gmail.com>
2022-01-25 17:22:39 +0900

    Deployed to Firebase. Fixed errors of ESlint and Prettier that appeared at the time of deployment.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,46 @@
 module.exports = {
-  env: {
+  'env': {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
+  'extends': [
+    'eslint:recommended',
     'google',
     'react-app',
     'react-app/jest',
   ],
-  parserOptions: {
+  'parserOptions': {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
-  rules: {
-    'operator-linebreak': ['error', 'before'],
-    'indent': [4, 4, { 'SwitchCase': 1 }],
+  'plugins': ['react'],
+  'rules': {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'indent': [0, 2, { SwitchCase: 1 }],
     'require-jsdoc': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     'object-curly-spacing': [2, 'always'],
-    'destructuring': 'all',
-    'space-before-function-paren': ['error', {'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always'}],
+    'destructuring': 'off',
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'always', named: 'never', asyncArrow: 'always' },
+    ],
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'operator-linebreak': [
       'error',
       'after',
       {
-        'overrides': {
+        overrides: {
           '?': 'before',
           ':': 'before',
-          '//': 'before'
-        }
-      }
-    ]
+          '//': 'before',
+        },
+      },
+    ],
   },
 };
